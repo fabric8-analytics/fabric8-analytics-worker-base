50a05f2c9723eac7f11e010c1cd1bf6e4e4a01ca
Michal Srb <michal@redhat.com>
2018-07-26 16:22:12 +0200

    Refactoring

--- hack/install_deps_rpm.sh
+++ hack/install_deps_rpm.sh
@@ -32,9 +32,14 @@ PCP="pcp"
 TAGGER_DEP="libarchive"
 
 # Install all RPM deps
-yum install -y --setopt=tsflags=nodocs ${BUILD} ${REQUIREMENTS_TXT} \
-                ${DB_MIGRATIONS} ${PROCESS_PY} \
-                ${DIGESTER} ${LINGUIST} \
-                ${OSCRYPTOCATCHER} ${CSMOCK_TASK_DEPS} \
-                ${BD_DEPS} ${BREWUTILS} ${MERCATOR} ${CODE_METRICS} \
-                ${DEPENDENCY_CHECK} ${GOLANG_SUPPORT} ${PCP} ${TAGGER_DEP}
+yum install -y --setopt=tsflags=nodocs \
+    ${BUILD} \
+    ${REQUIREMENTS_TXT} \
+    ${DB_MIGRATIONS} \
+    ${PROCESS_PY} \
+    ${DIGESTER} \
+    ${MERCATOR} \
+    ${GOLANG_SUPPORT} \
+    ${PCP} \
+    ${TAGGER_DEP}
+
